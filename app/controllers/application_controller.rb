40890b74084d51e071c99764a89de6a6e6ea9471
sakshyamdahal <dahalsakshyam@gmail.com>
2014-05-19 09:45:08 -0700

    finishing the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :get_current_time
+  before_action :authorize
 
   def get_current_time
   	@time = Time.now.to_formatted_s(:short)
   end
 
+  protected
+  	def authorize
+  		unless User.find_by(id: session[:user_id])
+  			redirect_to login_url, notice: "Please login"
+  		end
+  	end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,16 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "depot.com",
+    authentication: "plain",
+    user_name: "emailaddress@gmail.com",
+    password: "emailpassword",
+    enable_starttls_auto: true
+  }
 end
